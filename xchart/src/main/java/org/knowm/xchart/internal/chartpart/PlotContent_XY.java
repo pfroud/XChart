2ce7be8eec1fdb8c1d2bcac4087787a1ca884591
Peter Froud <pfroud@erp-power.com>
2021-01-07 01:00:06 -0800

    re-add imports

--- xchart/src/main/java/org/knowm/xchart/internal/chartpart/PlotContent_XY.java
+++ xchart/src/main/java/org/knowm/xchart/internal/chartpart/PlotContent_XY.java
@@ -1,6 +1,8 @@
 package org.knowm.xchart.internal.chartpart;
 
 import java.awt.Graphics2D;
+import java.awt.LinearGradientPaint;
+import java.awt.Paint;
 import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
